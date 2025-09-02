8e73908ef6a03825df068229ae92ee8f7b0ab27b
jqydee <matti.fischbach@web.de>
2025-09-02 12:01:11 +0200

    Initial Commit

---
+++ WindowSwitcher/WindowSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowSwitcherApp.swift
+//  WindowSwitcher
+//
+//  Created by Matti Fischbach on 02.09.25.
+//
+
+import SwiftUI
+
+@main
+struct WindowSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
